db9c860760937f32e2089adb03e0c6ddbd94ade4
Ahashm <ahmat18@student.sdu.dk>
2023-04-09 16:54:03 +0200

    Simple UI for Home Page

--- NodeJs/frontend/src/app/home/home.component.ts
+++ NodeJs/frontend/src/app/home/home.component.ts
@@ -3,8 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
+  alarmOn: boolean = false;
+  lockOn: boolean = false;
+  alarmName: string = 'Smart Lock System';
 
+  toggleAlarm() {
+    this.alarmOn = !this.alarmOn;
+    this.lockOn = this.alarmOn;
+  }
+
+  getAlarmText() {
+    return this.alarmOn ? 'Locked' : 'Unlocked';
+  }
+
+  getAlarmColor() {
+    return this.alarmOn ? 'red' : 'green';
+  }
 }
