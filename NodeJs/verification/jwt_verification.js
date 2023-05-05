b401da8f675363e53bf2908793927e2decf80529
Frederikh1 <42929762+Frederikh1@users.noreply.github.com>
2023-05-05 10:19:35 +0200

    Create jwt_verification.js

---
+++ NodeJs/verification/jwt_verification.js
@@ -0,0 +1,16 @@
+const jwt = require("jsonwebtoken");
+
+// Middleware to verify JWT token
+exports.verifyToken = (req, res, next) => {
+    const token = req.headers.authorization;
+    if (!token) {
+        return res.status(401).json({ message: "No token provided" });
+    }
+    try {
+        const decoded = jwt.verify(token, "secret_key");
+        req.userId = decoded.userId;
+        next();
+    } catch (err) {
+        return res.status(401).json({ message: "Invalid token" });
+    }
+};
