3c96a0abe4f8697aaddb5707f33523d93ca235ab
Frederikh1 <42929762+Frederikh1@users.noreply.github.com>
2023-05-05 11:02:10 +0200

    Update lock.js

--- NodeJs/routes/lock.js
+++ NodeJs/routes/lock.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const mqtt = require("../service/mqtt_client");
 const verifyToken = require("../verification/jwt_verification").verifyToken;
 
-//mangler at sende timestamp med, hvordan skal dette klares?
+//missing timestamp in these two endponts, as well as the validation of said timestamp. Also needs to send timestamp to mqtt
 
 router.post('/unlock', verifyToken, function (req, res) {
     interactWithLock(req, "unlock");
@@ -19,6 +19,12 @@ function interactWithLock(req, message) {
     let userId = req.body.userId;
     let lockId = req.body.lockId;
     mqtt.publish(userId, lockId, message);
+}
+
+function isAcceptableTime(time){
+    let receivedDate = new Date(time);
+    let now = Date.now();
+    return receivedDate < now;
 }
 
 module.exports = router;
