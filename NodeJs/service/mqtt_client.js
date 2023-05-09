e2bffbb4ebd8f208c7d9dc31220263476a5394b7
Frederikh1 <42929762+Frederikh1@users.noreply.github.com>
2023-05-09 10:08:14 +0200

    removed unused code

--- NodeJs/service/mqtt_client.js
+++ NodeJs/service/mqtt_client.js
@@ -15,11 +15,6 @@ const idPosition = () => {
   return lockTopicPath.split("/").indexOf("+");
 };
 
-/*
-client.on('message', (topic, payload) => {
-  console.log('Received Message:', topic, payload.toString())
-}) */
-
 exports.connect = (clientId) => {
   const connectUrl = `mqtt://${host}:${porti}`
   client = mqtt.connect(connectUrl, {
@@ -71,43 +66,3 @@ exports.publish = (userId, lockId, message) => {
 function createTopicPath(lock) {
   return `lock/${lock}/actions`;
 }
-
-//Basic example of mqtt subscribe
-
-/*
-client.subscribe([topic], () => {
-  console.log(`Subscribe to topic '${topic}'`)
-})
-*/
-
-/*const mqtt = require('mqtt')
-
-const host = '192.168.138.91'
-const port = '1885'
-const clientId = `mqtt_${Math.random().toString(16).slice(3)}`
-
-const connectUrl = `mqtt://${host}:${port}`
-const client = mqtt.connect(connectUrl, {
-  //clientId,
-  clean: true,
-  //connectTimeout: 4000,
-  //username: 'emqx',
-  //password: 'public',
-  reconnectPeriod: 1000,
-})
-
-const topic = '/esp32/ahash'
-client.on('connect', () => {
-  console.log('Connected')
-  client.subscribe([topic], () => {
-    console.log(`Subscribe to topic '${topic}'`)
-  })
-  client.publish(topic, 'nodejs mqtt test', { qos: 0, retain: false }, (error) => {
-    if (error) {
-      console.error(error)
-    }
-  })
-})
-client.on('message', (topic, payload) => {
-  console.log('Received Message:', topic, payload.toString())
-})*/
